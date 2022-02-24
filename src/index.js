dd7a494910a83507ff11aa7e525a0076bfb037f2
4otka <pocakeotakedjkewow@mail.ru>
2022-02-24 14:49:36 +0300

    added skobki

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let skobki = bracketsConfig.join('').replace(/,/g, '');
+    let stack = [];
+
+    for (let skobka of str) {
+        let bracketsIndex = skobki.indexOf(skobka)
+
+        if (bracketsIndex % 2 === 0) {
+            if (skobka === skobki[bracketsIndex + 1] && stack[stack.length - 1] === bracketsIndex){
+                stack.pop();
+            } else if (skobka === skobki[bracketsIndex + 1] && stack[stack.length - 1] !== bracketsIndex) {
+                stack.push(bracketsIndex)
+            }
+            else{
+                stack.push(bracketsIndex)
+            }
+        }
+        else {
+            if (stack.pop() !== bracketsIndex-1){
+                return false;
+            }
+        }
+
+    }
+
+    return stack.length === 0
 }
